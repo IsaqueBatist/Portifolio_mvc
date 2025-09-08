ce8710ee3090269e1b7992463f33acef54527276
Isaque Barbosa <isaquebatista400@gmail.com>
2025-09-08 09:12:19 -0300

    :wrench: chore: Configuring config.php

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'base_url' => 'http://localhost/projetos/Portifolio_mvc/public/',
+    'db_host' => 'localhost',
+    'db_name' => 'portifolio',
+    'db_user' => 'root',
+    'db_pass' => ''
+];
